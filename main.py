0f9b7c92a1437e5f1a7d8b14383ef80a31d46a14
citrazizah <azizahcitra012@gmail.com>
2019-05-09 14:06:43 +0700

    done

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import KFold
 
-iris_data = pd.read_csv('iris.csv')
-print(iris_data)
 
 def count_error(X_train, X_test, y_train, y_test):
     knn.fit(X_train, y_train)
@@ -36,6 +34,25 @@ def loo_errorratio(data):
 
 def kfold_errorratio(data):
     kfold = KFold(n_split = 10)
+    
+    X = np.array(data)[:,:4]
+    Y = np.array(data)[:,4]
+
+    error = 0
+    for train_index, test_index in kfold.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+       
+        if (count_error(X_train, X_test, y_train, y_test)):
+            error += 1
+
+    print('Error with KFOLD : ', (error / len(data)) * 100, '%')
 
     
+
+iris_data = pd.read_csv('iris.csv').tolist()
+print(iris_data)
 knn = KNeighborsClassifier(n_neighbors=3)
+loo_errorratio(iris_data)
+kfold_errorratio(iris_data)
+
