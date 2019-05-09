79b2dbe2b56c0fad7e5d69247da913a96b78cf72
DufanD <quraishdufan@rocketmail.com>
2019-05-09 14:52:39 +0700

    fixing kfold

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import KFold
 
-
 def count_error(X_train, X_test, y_train, y_test):
     knn.fit(X_train, y_train)
     prediksi = knn.predict(X_test)
@@ -16,11 +15,10 @@ def count_error(X_train, X_test, y_train, y_test):
     return False
 
 def loo_errorratio(data):
-
     loo = LeaveOneOut()
 
-    X = np.array(data)[:,:4]
-    Y = np.array(data)[:,4]
+    X = data[:, :4]
+    y = data[:, 4]
 
     error = 0
     for train_index, test_index in loo.split(X):
@@ -33,26 +31,24 @@ def loo_errorratio(data):
     print('Error with LOO : ', (error / len(data)) * 100, '%')
 
 def kfold_errorratio(data):
-    kfold = KFold(n_split = 10)
+    kfold = KFold(n_splits = 10)
     
-    X = np.array(data)[:,:4]
-    Y = np.array(data)[:,4]
+    X = data[:, :4]
+    y = data[:, 4]
 
     error = 0
     for train_index, test_index in kfold.split(X):
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
-       
-        if (count_error(X_train, X_test, y_train, y_test)):
-            error += 1
 
+        for i, test_data in enumerate(X_test):
+          if (count_error(X_train, [test_data.tolist()], y_train, y_test[i])):
+            error += 1
+        
     print('Error with KFOLD : ', (error / len(data)) * 100, '%')
 
-    
+iris_data = np.array(pd.read_csv('iris.csv'))
 
-iris_data = pd.read_csv('iris.csv').tolist()
-print(iris_data)
 knn = KNeighborsClassifier(n_neighbors=3)
-loo_errorratio(iris_data)
 kfold_errorratio(iris_data)
-
+loo_errorratio(iris_data)
