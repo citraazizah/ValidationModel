b9abfb8dc93a29d66f709f2bd72cdfded66b41fc
DufanD <quraishdufan@rocketmail.com>
2019-05-13 20:18:56 +0700

    remove unused import

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #%%stage
 import pandas as pd  
 import numpy as np 
-from scipy import stats
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import LeaveOneOut
 from sklearn.model_selection import KFold
